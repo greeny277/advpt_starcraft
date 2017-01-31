6fa31b5158a9c8c6c5c9306d5ddd645baec67005
Christian Bay <christian.bay@studium.fau.de>
2017-01-31 14:38:26 +0100

    dot.sh creates pdf from now on

--- src/dot.sh
+++ src/dot.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-./build.sh && build/SC2BuildSimulator dump "$1" > "$1.dot" && dot -T ps "$1.dot" > "$1.ps"
+./build.sh && build/SC2BuildSimulator dump "$1" > "$1.dot" && dot -T pdf "$1.dot" > "$1.pdf"
